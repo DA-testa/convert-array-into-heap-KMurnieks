897156bd38f86feb391f87371695474f87ec214e
Kristaps Murnieks <118051283+KMurnieks@users.noreply.github.com>
2023-03-18 14:30:34 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,14 +2,32 @@
 
 
 def build_heap(data):
+    d = len(data)
     swaps = []
+    for i in range (d/2-1, -1, -1):
+        swaps = swaps + heaping(data, i ,d)
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
     return swaps
-
-
+    
+    def heaping(data, i):
+        swaps = []
+        left = 2*i+1
+        right = 2 * i +2
+        smallest = i
+        if left < d and data[left] < data[smallest]:
+            smallest = left
+        if right < d and data[right] < data[smalleest]:
+            smallest = right
+        
+        if smallest != i:
+            swaps.append((i, smallest))
+            data[i], data[smallest] = data[smallest], data[i]
+            swamps = swaps heaping(data, smallest)
+            
+        return swaps
+            
+            
 def main():
     
     # TODO : add input and corresponding checks
