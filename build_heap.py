81f0cffebc0d080f1f9e411aaae8914e49dc8422
Kristaps Murnieks <118051283+KMurnieks@users.noreply.github.com>
2023-03-18 15:07:59 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -17,9 +17,9 @@ def heaping(data, i, swaps):
     return swaps
         
 def build_heap(data):
-    n = len(data)
+    x = len(data)
     swaps = []
-    for i in range (n // 2-1, -1, -1):
+    for i in range (x // 2-1, -1, -1):
         swaps = swaps + heaping(data, i ,swaps)
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
